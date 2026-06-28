da982e44466b0a16b74114a26da86f3e401221bc
Pradeep <lampinthesea224@gmail.com>
2026-06-29 00:54:08 +0530

    First commit

---
+++ Basic Python/Basics.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+cgpa = float(input("Enter your CGPA: "))
+
+print("My name is", name)
+print("My age is", age)
+print("My CGPA is", cgpa)
+
+print(f"My name is {name}, my age is {age}, and my CGPA is {cgpa}")
---
+++ Basic Python/oop.py
@@ -0,0 +1,7 @@
+class car:
+    pass
+car1 = car()
+car2 = car()
+
+print(car1)
+print(car2)
