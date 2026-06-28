92486fe7d4329ee453eff4bd6bdead12b6db5953
Pradeep <246137392+Pradeeeeeeeep@users.noreply.github.com>
2026-06-29 01:17:47 +0530

    oop defalut values

--- Python/oop.py
+++ Python/oop.py
@@ -1,13 +1,13 @@
 class Student:
-    def __init__(self, name, age, grade):
+    def __init__(self, name, age=21):
         self.name = name
         self.age = age
-        self.grade = grade
 
     def greet(self):
         print(f"Hello, my name is {self.name}")
+        print(f"And I am {self.age} years old.")
 
-Student1 = Student("John", 20, "A")
-Student2 = Student("Alice", 22, "B")
+Student1 = Student("John", 24)
+Student2 = Student("Alice")
 Student1.greet()
 Student2.greet()
